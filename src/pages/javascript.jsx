2920ba7fa147febb33f1baf9d9aa9f69643addd0
earthtojake <jakehenryfitz@gmail.com>
2022-05-26 21:09:20 +1000

    Restructure

--- src/pages/javascript.jsx
+++ src/pages/javascript.jsx
@@ -12,52 +12,36 @@ import {
   StatLabel,
   StatNumber,
   Text,
-} from '@chakra-ui/react'
-// Lyra.js - you will need Market, Board, Strike types for this task
-import Lyra, { Deployment } from '@lyrafinance/lyra-js'
-import { utils } from 'ethers'
-import React, { useEffect, useState } from 'react'
+} from '@chakra-ui/react';
+import Lyra, { Deployment } from '@lyrafinance/lyra-js';
+import { utils } from 'ethers';
+import React, { useEffect, useState } from 'react';
 
-// Format $USD into a string
-const formatUSD = usd => (isNaN(usd) ? `?` : `$${usd.toFixed(2)}`)
+const lyra = new Lyra(Deployment.Kovan);
 
-// Format an epoch timestamp into a string
-const formatDate = ts => {
-  const date = new Date(ts * 1000)
-  return date.toLocaleString(`default`, { month: `long`, day: `2-digit` })
-}
-
-// Converts a BigNumber to number (for rendering)
-const fromBigNumber = bn => parseFloat(utils.formatUnits(bn.toString(), 18))
-
-// Converts a number to BigNumber (for SDK arguments)
-const toBigNumber = val => utils.parseEther(val.toString())
-
-const lyra = new Lyra(Deployment.Kovan)
-
-const MARKET_NAME = `ETH`
+const MARKET_NAME = `ETH`;
 
 export default function Home() {
-  const [market, setMarket] = useState(null)
+  const [market, setMarket] = useState(null);
 
   useEffect(() => {
     // Fetch the Market entity from Lyra.js
-    lyra.market(MARKET_NAME).then(market => {
-      setMarket(market)
-    })
-  }, [])
+    lyra.market(MARKET_NAME).then((market) => {
+      setMarket(market);
+    });
+  }, []);
 
-  const strikePrice = null
-  const expiryTimestamp = null
-  const spotPrice = market ? fromBigNumber(market.spotPrice) : null
+  const strikePrice = null;
+  const expiryTimestamp = null;
+  const spotPrice = market ? fromBigNumber(market.spotPrice) : null;
 
-  const isCall = strikePrice && spotPrice ? strikePrice > spotPrice : false
+  const isCall = strikePrice && spotPrice ? strikePrice > spotPrice : false;
 
   if (!market || !spotPrice) {
-    return <Spinner size="xl" />
+    return <Spinner size="xl" />;
   }
 
-  const premium = null
+  const premium = null;
 
   return (
     <>
@@ -69,7 +53,9 @@ export default function Home() {
         <Text>
           I think {MARKET_NAME} is going {isCall ? `up` : `down`} to&nbsp;
           <Menu>
-            <MenuButton as={Button}>{strikePrice ? formatUSD(strikePrice) : `?`}</MenuButton>
+            <MenuButton as={Button}>
+              {strikePrice ? formatUSD(strikePrice) : `?`}
+            </MenuButton>
             <MenuList>
               <MenuItem>A</MenuItem>
               <MenuItem>B</MenuItem>
@@ -80,14 +66,34 @@ export default function Home() {
         </Text>
       </Flex>
       <Divider mb={4} width={500} />
-      <Stat sx={{ border: `1px solid lightgray`, borderRadius: `16px`, width: 250 }} p={4}>
+      <Stat
+        sx={{ border: `1px solid lightgray`, borderRadius: `16px`, width: 250 }}
+        p={4}
+      >
         <StatLabel>
           Buy {MARKET_NAME} {strikePrice ? formatUSD(strikePrice) : '?'}&nbsp;
           {isCall ? `Call` : `Put`}
         </StatLabel>
-        <StatNumber>{premium ? formatUSD(premium) : '?'}</StatNumber>
-        <StatHelpText>Exp. {expiryTimestamp ? formatDate(expiryTimestamp) : '?'}</StatHelpText>
+        <StatNumber>Premium: {premium ? formatUSD(premium) : '?'}</StatNumber>
+        <StatHelpText>
+          Expires {expiryTimestamp ? formatDate(expiryTimestamp) : '?'}
+        </StatHelpText>
       </Stat>
     </>
-  )
+  );
 }
+
+// Format $USD into a string
+const formatUSD = (usd) => (isNaN(usd) ? `?` : `$${usd.toFixed(2)}`);
+
+// Format an epoch timestamp into a string
+const formatDate = (ts) => {
+  const date = new Date(ts * 1000);
+  return date.toLocaleString(`default`, { month: `long`, day: `2-digit` });
+};
+
+// Converts a BigNumber to number (for rendering)
+const fromBigNumber = (bn) => parseFloat(utils.formatUnits(bn.toString(), 18));
+
+// Converts a number to BigNumber (for SDK arguments)
+const toBigNumber = (val) => utils.parseEther(val.toString());
